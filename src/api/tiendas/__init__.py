5dd20912b429cb4258db2f138579d3d6878ac9ed
Carlos Eduardo <routeres@gmail.com>
2026-01-17 20:57:12 -0500

    tablas de calificaciòn de usuarios

--- src/api/tiendas/__init__.py
+++ src/api/tiendas/__init__.py
@@ -1,2 +1,8 @@
+"""
+Módulo de Tiendas Online - TuKomercio
+Contiene APIs para checkout y gestión de pedidos de tiendas online
+"""
+
 from .checkout_api import checkout_api_bp
+
 __all__ = ['checkout_api_bp']
